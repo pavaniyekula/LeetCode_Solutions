949764612e7a6a82f1ca8e7583da96ec624974df
pavaniyekula <142112190+pavaniyekula@users.noreply.github.com>
2024-09-23 10:55:00 +0530

    Time: 19 ms (30.35%), Space: 54.6 MB (99.59%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,26 +1,9 @@
 class Solution {
-    public boolean containsDuplicate(int[] nums) 
-    {
-        /*HashMap<Integer, Integer> freq=new HashMap<>();
-        for(int num:nums)
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++)
         {
-            freq.put(num,freq.getOrDefault(num,0)+1);
-        }
-        for(int num:nums)
-        {
-            if(freq.get(num)>1)
-                return true;
-        }
-        return false;*/
-        
-        HashMap<Integer,Integer> freq=new HashMap<>();
-        for(int i:nums)
-        {
-            freq.put(i,freq.getOrDefault(i,0)+1);
-        }
-        for(int j:freq.keySet())
-        {
-            if(freq.get(j)>1) return true;
+            if(nums[i]==nums[i-1]) return true;
         }
         return false;
     }
